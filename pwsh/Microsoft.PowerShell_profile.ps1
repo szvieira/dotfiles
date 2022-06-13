eba2d0eef7e616f6b9033cec80ec268940dc87ed
Gustavo Vieira <guga9055@gmail.com>
2022-06-13 09:54:31 -0300

    feat: vscode and powershell configuration's files

---
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,25 @@
+Import-Module PSReadLine
+
+oh-my-posh --init --shell pwsh --config C:\CONFIGS\kali.omp.json | Invoke-Expression
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+Set-Alias -Name n -Value nvim
+Set-Alias -Name j -Value z
+
+if ($host.Name -eq 'ConsoleHost') {
+    function ls_git { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args }
+    Set-Alias -Name ls -Value ls_git -Option Private
+}
+
+function .. {
+    cd ..
+}
+
+Set-PSReadLineOption -PredictionSource History
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+echo "Great, the digital pimp at work."
