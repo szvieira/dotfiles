ce10a722895756bc7609ca8ddf475c3a50d10b96
Gustavo Vieira <guga9055@gmail.com>
2022-11-14 09:58:29 -0300

    config of oh-my-posh and README

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -7,19 +7,5 @@ if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
 
-Set-Alias -Name n -Value nvim
-Set-Alias -Name j -Value z
-
-if ($host.Name -eq 'ConsoleHost') {
-    function ls_git { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args }
-    Set-Alias -Name ls -Value ls_git -Option Private
-}
-
-function .. {
-    cd ..
-}
-
-Set-PSReadLineOption -PredictionSource History
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 echo "Great, the digital pimp at work."
